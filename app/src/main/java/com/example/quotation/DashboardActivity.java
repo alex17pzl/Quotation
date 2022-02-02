5f338765b24988a5db58b4ff062cce11c04bb03c
Alejandro Avila Solera <alex_avila_solera@hotmail.es>
2022-02-02 18:44:00 +0100

    Ejercicio 2 resuleto

--- app/src/main/java/com/example/quotation/DashboardActivity.java
+++ app/src/main/java/com/example/quotation/DashboardActivity.java
@@ -2,7 +2,12 @@ package com.example.quotation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button buttonGetQuotations = findViewById(R.id.getQuotation);
+        Button buttonFavouriteQuotations = findViewById(R.id.favouriteQuotations);
+        Button buttonSettings = findViewById(R.id.settings);
+        Button buttonAbout = findViewById(R.id.about);
+
+        View.OnClickListener x = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (buttonGetQuotations.isPressed()) {
+                    Toast.makeText(DashboardActivity.this, buttonGetQuotations.getText() , Toast.LENGTH_SHORT).show();
+                }
+                if (buttonFavouriteQuotations.isPressed()) {
+                    Toast.makeText(DashboardActivity.this, buttonFavouriteQuotations.getText() , Toast.LENGTH_SHORT).show();
+                }
+                if (buttonSettings.isPressed()) {
+                    Toast.makeText(DashboardActivity.this, buttonSettings.getText() , Toast.LENGTH_SHORT).show();
+                }
+                if (buttonAbout.isPressed()) {
+                    Intent intent = new Intent(DashboardActivity.this, AboutActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+        };
+
+        buttonGetQuotations.setOnClickListener(x);
+        buttonFavouriteQuotations.setOnClickListener(x);
+        buttonSettings.setOnClickListener(x);
+        buttonAbout.setOnClickListener(x);
     }
 }
